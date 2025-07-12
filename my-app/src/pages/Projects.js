be5594134d18e54716f74f55ca2f7801e941b4b2
tadarsh <adarshsomeshwaralok@gmail.com>
2025-07-12 08:48:48 +0530

    added links for github repo

--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -28,22 +28,22 @@ const Projects = () => {
       title: "AI Notes Summarizer",
       description: "A web app that summarizes academic notes using AI-backed APIs.",
       tech: "Django, React, PostgreSQL",
-      github: "https://github.com/tadarshpandey/ai-notes-summarizer",
+      github: "https://github.com/tadarshpandey/ai_notes_portal",
       demo: "https://ai-notes-frontend.onrender.com/",
     },
     {
       title: "Chatbot Web App",
       description: "An open-source chatbot redeployed using Streamlit.",
       tech: "Python, Streamlit",
-      github: "https://github.com/tadarshpandey/chatbot-app",
+      github: "https://github.com/tadarshpandey/chatbot",
       demo: "https://adarshpandeyind-chatbot.streamlit.app/",
     },
     {
       title: "Real Estate Website",
       description: "A property listing website with admin features and search.",
       tech: "Django, HTML/CSS, MySQL",
-      github: "https://github.com/tadarshpandey/real-estate",
-      demo: "#", // add live demo link if available
+      github: "https://github.com/tadarshpandey/project_shre",
+      //demo: "#", // add live demo link if available
     },
   ];
 
